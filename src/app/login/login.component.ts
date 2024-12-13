3e34a1c680aa98b13a0884075a81c1b0c07bf38b
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-12-13 17:30:50 +0100

    login fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private fb: FormBuilder, private loginService: LoginService) {}
+  loginForm !: FormGroup
+
+  buildLoginForm(){
+    this.loginForm = this.fb.group(
+      {
+        email: new FormControl("", Validators.required),
+        password: new FormControl("", Validators.required)
+      }
+    )
+  }
+  ngOnInit(): void {
+    this.buildLoginForm()
+  }
+
+
+  login(){
+    const email = this.loginForm.get("email")?.value;
+    const password = this.loginForm.get("password")?.value; 
+    const body = {
+      email: email,
+      password: password
+    }
+    this.loginService.login(body).subscribe((response: any) => {
+      this.navigateToTodo()
+    }, (error: any) => console.error(error))
+
+    
+  }
 
   navigateToTodo() {
     this.router.navigate(['/todo-list']);
